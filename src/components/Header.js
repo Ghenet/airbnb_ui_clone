3694b274bee130e372d144307a8273e9280a43ab
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-26 13:23:30 -0700

    adds the searchbar and search icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "../css/Header.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
-    <div>
+    <div className="header">
       <img
         className="header__icon"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQp-nYTWgFoqRnmFEofLfLFyyqJo52LzMv0Bg&usqp=CAU"
       />
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
     </div>
   );
 }
