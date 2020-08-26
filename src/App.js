edc7c31466eef35adde5fe7106cd3910b0fe6b16
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-26 12:53:08 -0700

    adds the header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       {/*Home*/}------
+      <Header />
       <Home />
       {/* Header*/}
       {/*Banner*/}
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/Header.css";
+
+function Header() {
+  return (
+    <div>
+      <img
+        className="header__icon"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQp-nYTWgFoqRnmFEofLfLFyyqJo52LzMv0Bg&usqp=CAU"
+      />
+    </div>
+  );
+}
+
+export default Header;
