9d47accacf33e0332f2149e903e4e4a431ea5bf2
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-26 12:32:26 -0700

    creates the home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="app">
       {/*Home*/}------
+      <Home />
       {/* Header*/}
       {/*Banner*/}
       {/*Cards*/}
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/Home.css";
+
+function Home() {
+  return <div className="home">This is home.</div>;
+}
+
+export default Home;
